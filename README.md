71010f97e4308e9516869bb45807d1960c750696
Jin <jin@futunn.com>
2016-11-28 16:49:14 +0800

    add tree-worker

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # tree-worker
 iterator files in Node.js 
+
+```
+  npm install --save tree-worker
+```
+
+## use
+```js
+  var treeWorker = new TreeWorker('C:\\Users\\Administrator.05121115\\Desktop\\demo');
+
+  treeWorker.work().then((that)=>{   //init
+      console.log('stat: ',that.getStat());
+  });
+  //or
+  treeWorker.work('C:\\Users\\Administrator.05121115\\Desktop\\__MACOSX\\demo').then((that)=>{
+      console.log('stat: ',that.stat);
+  });
+```
+
+## struct
+
+```js
+   {
+      'C:\Users\Administrator.05121115\Desktop\__MACOSX\demo':
+       { type: 'S_IFDIR',
+         paths:
+          { root: 'C:\\',
+            dir: 'C:\\Users\\Administrator.05121115\\Desktop\\__MACOSX',
+            base: 'demo',
+            ext: '',
+            name: 'demo' },
+         origin: 'C:\\Users\\Administrator.05121115\\Desktop\\__MACOSX\\demo',
+         encoding: 'utf-8',
+         stats:
+          { dev: -64860894,
+            mode: 16822,
+            nlink: 1,
+            uid: 0,
+            gid: 0,
+            rdev: 0,
+            blksize: undefined,
+            ino: 41658296553398184,
+            size: 0,
+            blocks: undefined,
+            atime: 2016-11-14T03:38:10.427Z,
+            mtime: 2016-11-10T15:10:14.000Z,
+            ctime: 2016-11-14T03:38:10.537Z,
+            birthtime: 2016-11-14T03:38:10.427Z },
+         files: [ '._.DS_Store' ],
+         childrens: [ 'C:\\Users\\Administrator.05121115\\Desktop\\__MACOSX\\demo\\._.DS_Store' ] },
+        'C:\Users\Administrator.05121115\Desktop\__MACOSX\demo\._.DS_Store':
+       { type: 'S_IFREG',
+         paths:
+          { root: 'C:\\',
+            dir: 'C:\\Users\\Administrator.05121115\\Desktop\\__MACOSX\\demo',
+            base: '._.DS_Store',
+            ext: '.DS_Store',
+            name: '._' },
+         origin: 'C:\\Users\\Administrator.05121115\\Desktop\\__MACOSX\\demo\\._.DS_Store',
+         encoding: 'utf-8',
+         stats:
+          { dev: -64860894,
+            mode: 33206,
+            nlink: 1,
+            uid: 0,
+            gid: 0,
+            rdev: 0,
+            blksize: undefined,
+            ino: 18858823439835436,
+            size: 120,
+            blocks: undefined,
+            atime: 2016-11-14T03:38:10.427Z,
+            mtime: 2016-07-12T15:46:34.000Z,
+            ctime: 2016-11-14T03:38:10.428Z,
+            birthtime: 2016-11-14T03:38:10.427Z },
+         files: [],
+         childrens: [] 
+        } 
+    }
+```
---
+++ index.js
@@ -0,0 +1,165 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+/*const S_IFMT   = 0xf000; //bit mask for the file type bit field
+const S_IFSOCK = 0xc000; //socket
+const S_IFLNK  = 0xa000; //symbolic link
+const S_IFREG  = 0x8000; //regular file
+const S_IFBLK  = 0x6000; //block device
+const S_IFDIR  = 0x4000; //directory
+const S_IFCHR  = 0x2000; //character device
+const S_IFIFO  = 0x1000; //FIFO*/
+
+let getFileType = function(stats){
+    if ( stats.isFile() ) {
+        return 'S_IFREG';
+    }
+    if ( stats.isDirectory() ) {
+        return 'S_IFDIR';
+    }
+    if ( stats.isBlockDevice() ) {
+        return 'S_IFBLK';
+    }
+    if ( stats.isCharacterDevice() ) {
+        return 'S_IFCHR';
+    }
+    if ( stats.isFIFO() ) {
+        return 'S_IFIFO';
+    }
+    if ( stats.isSocket() ) {
+        return 'S_IFSOCK';
+    }
+    if ( stats.isSymbolicLink() ) {
+        return 'S_IFLNK';
+    }
+    return 'S_UNKOWN';
+};
+
+let fstats = function(pt){
+    return new Promise((resolve , reject)=>{
+        fs.stat(path.normalize(pt) , (err , stats)=>{
+            if ( err ) return reject(err);
+            return resolve(stats);
+        });
+    });
+};
+
+let fileInfo = function(resource){
+    return new Promise((resolve , reject)=>{
+        try{
+            return resolve({
+                type : resource.type,
+                paths : path.parse(resource.path),
+                origin: resource.path,
+                encoding : 'utf-8',
+                stats : resource.stats,
+                files : resource.files,
+                childrens : []
+            });
+        }catch(e){
+            return reject(e);
+        }
+    });
+};
+
+let freaddir = function(pt){
+    return new Promise((resolve , reject)=>{
+        fs.readdir(path.normalize(pt) , (err , files)=>{
+            if ( err ) return reject(err);
+            return resolve(files);
+        });
+    });
+};
+
+let reader = function(pt){
+    let resource = {};
+    return  fstats(pt)
+            .then((stats)=>{
+                resource.type = getFileType(stats);
+                resource.stats = stats;
+                return pt;
+            })
+            .then(freaddir)
+            .then((files)=>{
+                return files;
+            },()=>{
+                return [];
+            })
+            .then((files)=>{
+                resource.files = files;
+                resource.path = pt;
+                return resource;
+            })
+            .then(fileInfo);
+};
+
+let workerIterator = function(stat){
+    let ps = [];
+    if ( stat.files.length > 0 ) {
+        for ( let i = 0 ; i < stat.files.length ; i++ ){
+            ps.push(reader(path.resolve(stat.origin , stat.files[i])));
+        }
+    }
+    return Promise.all(ps)
+            .then((stats)=>{
+                return {stat:stat , childrens:stats};
+            }).then(concatWorker);
+};
+
+let concatWorker = function(resource){
+    let stat = resource.stat , childrens = resource.childrens;
+
+    let stats = [];
+
+    for (let i = 0 ; i < childrens.length ; i++ ){
+        stats.push(childrens[i]);
+        stat.childrens.push(childrens[i].origin);
+    }
+
+    return {stat : stat , stats : stats};
+};
+
+let concatStat = function(stat){
+    return (resource)=>{
+        stat[resource.stat.origin] = resource.stat;
+        for (let i = 0 ; i < resource.stats.length ; i++ ){
+            stat[resource.stats[i].origin] = resource.stats[i];
+        }
+        return stat;
+    };
+};
+
+let workerProcess = function(that , pt){
+    let concatStatFun = concatStat(that.stat);
+    return reader(pt ? pt : that.path).then(workerIterator).then(concatStatFun);
+};
+
+class TreeWorker{
+    constructor(pt){
+        this.path = pt;
+        this.stat = {};
+        this.isInit = false;
+    }
+    setStat(stat){
+        this.stat = stat;
+    }
+    getStat(){
+        return this.stat;
+    }
+    work(pt){
+        let _this = this;
+        if ( this.isInit ) {
+            return workerProcess(this , pt).then(()=>{return _this;});
+        }else if ( pt ){
+            return workerProcess(this).then(()=>{_this.isInit = true;}).then(()=>{
+                return workerProcess(this ,pt);
+            }).then(()=>{return _this;});
+        }else{
+            return workerProcess(this).then(()=>{_this.isInit = true;}).then(()=>{return _this;});
+        }
+    }
+}
+
+module.exports = TreeWorker;
